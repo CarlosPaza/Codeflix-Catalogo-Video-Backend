69c3c3d7e848767f341243302c541d8efcc91350
Carlos Paza <carlos_paza@hotmail.com>
2021-04-22 20:22:50 -0300

    commit inicial

---
+++ app/Models/Genre.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Genre extends Model
+{
+    use SoftDeletes, Traits\Uuid;
+
+    public $incrementing = false;
+    protected $fillable = ['name', 'is_active'];
+    protected $dates = ['deleted_at'];
+    protected $keyType = 'string';
+}
