1a4e782a94b848d7cf05bf3635a8e2ea93f81b47
Carlos Paza <carlos_paza@hotmail.com>
2021-04-26 21:59:25 +0000

    Ajustes no docker-compose

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 #On error no such file entrypoint.sh, execute in terminal - dos2unix .docker\entrypoint.sh
-cp .env.example .env
 chown -R www-data:www-data .
 composer install
 php artisan key:generate
